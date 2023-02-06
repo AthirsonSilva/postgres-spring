eeafd06bacca66c75320e04dd25fb526611f0295
Athirson <athirsonarceus@gmail.com>
2023-02-06 16:50:42 -0300

    Updating an existing customers endpoint

--- src/main/java/com/amigoscode/springboottutorial/Main.java
+++ src/main/java/com/amigoscode/springboottutorial/Main.java
@@ -22,6 +22,16 @@ public class Main {
 		SpringApplication.run(Main.class, args);
 	}
 
+	public Customer writeCustomer(Customer customer, NewCustomerRequest request) {
+		customer.setName(request.name());
+		customer.setEmail(request.email());
+		customer.setAge(request.age());
+
+		customerRepository.save(customer);
+
+		return customer;
+	}
+
 	@GetMapping("")
 	public Map<String, List<Customer>> getCustomers() {
 		HashMap<String, List<Customer>> map = new HashMap<>();
@@ -33,15 +43,9 @@ public class Main {
 	public Map<String, Object> registerNewCustomer(@RequestBody NewCustomerRequest request) {
 		Customer customer = new Customer();
 
-		customer.setName(request.name());
-		customer.setEmail(request.email());
-		customer.setAge(request.age());
-
-		customerRepository.save(customer);
-
 		// returns a JSON response
 		HashMap<String, Object> map = new HashMap<>();
-		map.put("Customer", customer);
+		map.put("Customer", writeCustomer(customer, request));
 		map.put("Message", "New customer successfully created!");
 
 		return map;
@@ -66,6 +70,22 @@ public class Main {
 						"Athirson Silva",
 						20,
 						"Software Engineer"));
+	}
+
+	@PatchMapping("/{id}")
+	public Map<String, Object> updateCustomer(
+			@PathVariable("id") Integer id,
+			@RequestBody NewCustomerRequest request
+	) {
+		Customer customer = customerRepository.findById(id).orElseThrow(
+				() -> new IllegalArgumentException("Customer with id " + id + " does not exist!"));
+
+
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("Customer", writeCustomer(customer, request));
+		map.put("Message", "Customer successfully updated!");
+
+		return map;
 	}
 
 	record NewCustomerRequest(
